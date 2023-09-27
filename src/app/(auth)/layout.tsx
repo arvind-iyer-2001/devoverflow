22a683ffe6f2c47b8b8c30fbccb032a59fb09105
Arvind Iyer <arvind.iyer.2001@gmail.com>
2023-09-28 02:52:18 +0530

    Add Auth

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="align-center justify-center ">{children}</div>;
+};
+
+export default AuthLayout;
